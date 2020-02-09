71f37c0b2cda5e77dedc951e4c6ab79d48696c1d
Kristofer Berggren <kristofer.berggren@gmail.com>
2020-02-09 14:26:15 +0800

    log libetpan smtp/imap error enum names instead of numbers

--- src/loghelp.h
+++ src/loghelp.h
@@ -34,106 +34,127 @@
                                   Log::Debug(__FILENAME__, __LINE__, "%s(%s)", __FUNCTION__, str.c_str()); \
                                 } while(0)
 #define LOG_DEBUG_VAR(MSG, VAR) do { if (!Log::GetDebugEnabled()) break; \
-                                     const std::string& str = PrettyPrint(VAR); \
+                                     const std::string& str = LogHelp::PrettyPrint(VAR); \
                                      LOG_DEBUG(MSG " %s", str.c_str()); \
                                    } while (0)
 
-#define LOG_IF_NULL(EXPR) LogIfEqual(EXPR, NULL, #EXPR, __FILENAME__, __LINE__)
-#define LOG_IF_SMTP_ERR(EXPR) LogIfNotEqual(EXPR, MAILSMTP_NO_ERROR, #EXPR, __FILENAME__, __LINE__)
-#define LOG_IF_IMAP_ERR(EXPR) LogImapHelper(EXPR, #EXPR, __FILENAME__, __LINE__)
-#define LOG_IF_IMAP_LOGOUT_ERR(EXPR) LogImapLogoutHelper(EXPR, #EXPR, __FILENAME__, __LINE__)
+#define LOG_IF_NULL(EXPR) LogHelp::LogIfEqual(EXPR, NULL, #EXPR, __FILENAME__, __LINE__)
+#define LOG_IF_IMAP_ERR(EXPR) LogHelp::LogImap(EXPR, #EXPR, __FILENAME__, __LINE__)
+#define LOG_IF_IMAP_LOGOUT_ERR(EXPR) LogHelp::LogImapLogout(EXPR, #EXPR, __FILENAME__, __LINE__)
+#define LOG_IF_SMTP_ERR(EXPR) LogHelp::LogSmtp(EXPR, #EXPR, __FILENAME__, __LINE__)
 
-static inline int LogImapHelper(int p_Rv, const char* p_Expr, const char* p_File, int p_Line) 
+class LogHelp
 {
-  if (p_Rv > MAILIMAP_NO_ERROR_NON_AUTHENTICATED)
-  {
-    Log::Error(p_File, p_Line, "%s = %d", p_Expr, p_Rv);
-  }
-  else
-  {
-    Log::Debug(p_File, p_Line, "%s = %d", p_Expr, p_Rv);
-  }
+public:
+  static std::string ImapErrToStr(int p_ImapErr);
+  static std::string SmtpErrToStr(int p_SmtpErr);
 
-  return p_Rv;
-}
+  static inline int LogImap(int p_Rv, const char* p_Expr, const char* p_File, int p_Line)
+  {
+    if (p_Rv > MAILIMAP_NO_ERROR_NON_AUTHENTICATED)
+    {
+      Log::Error(p_File, p_Line, "%s = %s", p_Expr, ImapErrToStr(p_Rv).c_str());
+    }
+    else if (Log::GetDebugEnabled())
+    {
+      Log::Debug(p_File, p_Line, "%s = %s", p_Expr, ImapErrToStr(p_Rv).c_str());
+    }
 
-static inline int LogImapLogoutHelper(int p_Rv, const char* p_Expr, const char* p_File,
-                                      int p_Line) 
-{
-  if ((p_Rv > MAILIMAP_NO_ERROR_NON_AUTHENTICATED) && (p_Rv != MAILIMAP_ERROR_STREAM))
-  {
-    Log::Error(p_File, p_Line, "%s = %d", p_Expr, p_Rv);
-  }
-  else
-  {
-    Log::Debug(p_File, p_Line, "%s = %d", p_Expr, p_Rv);
+    return p_Rv;
   }
 
-  return p_Rv;
-}
-
-template <typename T>
-struct identity { typedef T type; };
-
-template <typename T>
-static inline T LogIfNotEqual(T p_Rv, typename identity<T>::type p_Expect, const char* p_Expr,
-                              const char* p_File, int p_Line) 
-{
-  if (p_Rv != p_Expect)
+  static inline int LogImapLogout(int p_Rv, const char* p_Expr, const char* p_File, int p_Line)
   {
-    Log::Error(p_File, p_Line, "%s = 0x%x", p_Expr, p_Rv);
-  }
-  else
-  {
-    Log::Debug(p_File, p_Line, "%s = 0x%x", p_Expr, p_Rv);
+    if ((p_Rv > MAILIMAP_NO_ERROR_NON_AUTHENTICATED) && (p_Rv != MAILIMAP_ERROR_STREAM))
+    {
+      Log::Error(p_File, p_Line, "%s = %s", p_Expr, ImapErrToStr(p_Rv).c_str());
+    }
+    else if (Log::GetDebugEnabled())
+    {
+      Log::Debug(p_File, p_Line, "%s = %s", p_Expr, ImapErrToStr(p_Rv).c_str());
+    }
+
+    return p_Rv;
   }
 
-  return p_Rv;
-}
+  static inline int LogSmtp(int p_Rv, const char* p_Expr, const char* p_File, int p_Line) 
+  {
+    if (p_Rv != MAILSMTP_NO_ERROR)
+    {
+      Log::Error(p_File, p_Line, "%s = %s", p_Expr, SmtpErrToStr(p_Rv).c_str());
+    }
+    else if (Log::GetDebugEnabled())
+    {
+      Log::Debug(p_File, p_Line, "%s = %s", p_Expr, SmtpErrToStr(p_Rv).c_str());
+    }
 
-template <typename T>
-static inline T LogIfEqual(T p_Rv, typename identity<T>::type p_Expect, const char* p_Expr,
-                           const char* p_File, int p_Line) 
-{
-  if (p_Rv == p_Expect)
-  {
-    Log::Error(p_File, p_Line, "%s = 0x%x", p_Expr, p_Rv);
-  }
-  else
-  {
-    Log::Debug(p_File, p_Line, "%s = 0x%x", p_Expr, p_Rv);
+    return p_Rv;
   }
 
-  return p_Rv;
-}
+  template <typename T>
+  struct identity { typedef T type; };
 
-template <typename T>
-static inline std::string PrettyPrint(const T& p_Container)
-{
-  std::stringstream sstream;
-  sstream << p_Container;
-  return sstream.str();
-}
-
-void PrettyPrintArgsHelper(std::stringstream& /*p_Sstream*/);
-
-template<typename FirstArg, typename... RemainingArgs>
-void PrettyPrintArgsHelper(std::stringstream& p_Sstream, const FirstArg& p_FirstArg, const RemainingArgs&... p_RemainingArgs)
-{
-  if (!p_Sstream.str().empty())
+  template <typename T>
+  static inline T LogIfNotEqual(T p_Rv, typename identity<T>::type p_Expect, const char* p_Expr,
+                                const char* p_File, int p_Line) 
   {
-    p_Sstream << ", ";
+    if (p_Rv != p_Expect)
+    {
+      Log::Error(p_File, p_Line, "%s = 0x%x", p_Expr, p_Rv);
+    }
+    else if (Log::GetDebugEnabled())
+    {
+      Log::Debug(p_File, p_Line, "%s = 0x%x", p_Expr, p_Rv);
+    }
+
+    return p_Rv;
   }
 
-  p_Sstream << p_FirstArg;
+  template <typename T>
+  static inline T LogIfEqual(T p_Rv, typename identity<T>::type p_Expect, const char* p_Expr,
+                             const char* p_File, int p_Line) 
+  {
+    if (p_Rv == p_Expect)
+    {
+      Log::Error(p_File, p_Line, "%s = 0x%x", p_Expr, p_Rv);
+    }
+    else if (Log::GetDebugEnabled())
+    {
+      Log::Debug(p_File, p_Line, "%s = 0x%x", p_Expr, p_Rv);
+    }
 
-  PrettyPrintArgsHelper(p_Sstream, p_RemainingArgs...);
-}
+    return p_Rv;
+  }
+
+  template <typename T>
+  static inline std::string PrettyPrint(const T& p_Container)
+  {
+    std::stringstream sstream;
+    sstream << p_Container;
+    return sstream.str();
+  }
+
+  static void PrettyPrintArgsHelper(std::stringstream& /*p_Sstream*/);
+
+  template<typename FirstArg, typename... RemainingArgs>
+  static void PrettyPrintArgsHelper(std::stringstream& p_Sstream, const FirstArg& p_FirstArg,
+                                    const RemainingArgs&... p_RemainingArgs)
+  {
+    if (!p_Sstream.str().empty())
+    {
+      p_Sstream << ", ";
+    }
+
+    p_Sstream << p_FirstArg;
+
+    PrettyPrintArgsHelper(p_Sstream, p_RemainingArgs...);
+  }
+};
 
 template<typename... Args>
 std::string STR(const Args&... p_Args)
 {
   std::stringstream sstream;
-  PrettyPrintArgsHelper(sstream, p_Args...);
+  LogHelp::PrettyPrintArgsHelper(sstream, p_Args...);
   return sstream.str();
 }
